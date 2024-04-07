ca0fbb2272510f70a7b3c35c5dddf65dcf997f8c
joengatia <ngatiajoe2@gmail.com>
2024-04-07 13:40:02 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+}
+
+export { submitData };
